eadc8d8d29c66f2d90823f70b2d0135a27cf5557
Demis Bellot <demis.bellot@gmail.com>
2022-03-02 22:11:28 +0800

    Add OSS License Key

--- TalentBlazor/Configure.AppHost.cs
+++ TalentBlazor/Configure.AppHost.cs
@@ -23,7 +23,11 @@ public class AppHost : AppHostBase, IHostingStartup
         }, allowCredentials: true));
     }
 
-    public void Configure(IWebHostBuilder builder) => builder
-        .ConfigureServices((context, services) => 
-            services.ConfigureNonBreakingSameSiteCookies(context.HostingEnvironment));
+    public void Configure(IWebHostBuilder builder)
+    {
+        Licensing.RegisterLicense("OSS BSD-3-Clause 2022 https://github.com/NetCoreApps/TalentBlazor eRijOv1P5vqep+GPt26wpgS7wvOYekpC7d5ipolpjPmMq/7A7LRKKLL3uTqQnQo1ySi/Ms2crO/rd2Fyeh3IohisA7K7xYiCtMry+DKa41EJ6dZUDIU3wR0/dpHqVKao3UpO8+dawYjv/ok37ENcLo/zDwub609UcR8/ytl+3aw=");
+        builder
+            .ConfigureServices((context, services) =>
+                services.ConfigureNonBreakingSameSiteCookies(context.HostingEnvironment));
+    }
 }
