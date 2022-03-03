dcda726e02cf0d55a148aa3d43d8cbec7bb2b1f8
Demis Bellot <demis.bellot@gmail.com>
2022-03-03 14:29:12 +0800

    Remove Bookings + Hello + Todos

--- TalentBlazor.Tests/UnitTest.cs
+++ TalentBlazor.Tests/UnitTest.cs
@@ -13,19 +13,9 @@ public class UnitTest
     public UnitTest()
     {
         appHost = new BasicAppHost().Init();
-        appHost.Container.AddTransient<MyServices>();
+        appHost.Container.AddTransient<TalentServices>();
     }
 
     [OneTimeTearDown]
     public void OneTimeTearDown() => appHost.Dispose();
-
-    [Test]
-    public void Can_call_MyServices()
-    {
-        var service = appHost.Container.Resolve<MyServices>();
-
-        var response = (HelloResponse)service.Any(new Hello { Name = "World" });
-
-        Assert.That(response.Result, Is.EqualTo("Hello, World!"));
-    }
 }
