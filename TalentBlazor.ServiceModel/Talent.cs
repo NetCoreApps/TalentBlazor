48e93decaa4e3631d9532bdba0e2b7023a406b2c
Darren Reid <darren@reidmail.org>
2022-03-04 15:12:15 +1100

    Rename comments, trying to debug why CI isn't deploying changes.

--- TalentBlazor.ServiceModel/Talent.cs
+++ TalentBlazor.ServiceModel/Talent.cs
@@ -81,7 +81,7 @@ public class JobApplicationComment : AuditBase
 
     [References(typeof(JobApplication))]
     public int JobApplicationId { get; set; }
-    public string Message { get; set; }
+    public string Comment { get; set; }
 }
 
 [Icon(Svg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path fill='currentColor' d='M18 19H6v-1.4c0-2 4-3.1 6-3.1s6 1.1 6 3.1M12 7a3 3 0 0 1 3 3a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3m0-4a1 1 0 0 1 1 1a1 1 0 0 1-1 1a1 1 0 0 1-1-1a1 1 0 0 1 1-1m7 0h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2Z'/></svg>")]
@@ -458,7 +458,7 @@ public class CreateJobApplicationComment : ICreateDb<JobApplicationComment>, IRe
     public int JobApplicationId { get; set; }
     [ValidateNotEmpty]
     [Input(Type = "textarea"), FieldCss(Field = "col-span-12 text-center")]
-    public string Message { get; set; }
+    public string Comment { get; set; }
 }
 
 [Tag("Talent")]
@@ -472,7 +472,7 @@ public class UpdateJobApplicationComment : IUpdateDb<JobApplicationComment>, IRe
     public int JobApplicationId { get; set; }
     [ValidateNotEmpty]
     [Input(Type = "textarea"), FieldCss(Field = "col-span-12 text-center")]
-    public string Message { get; set; }
+    public string Comment { get; set; }
 }
 
 [Tag("Talent")]
