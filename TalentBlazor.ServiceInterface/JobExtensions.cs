3a5726496b9de684015b49feee362e4afc74486f
Demis Bellot <demis.bellot@gmail.com>
2022-03-08 14:39:42 +0800

    Fix StackOverflow

--- TalentBlazor.ServiceInterface/JobExtensions.cs
+++ TalentBlazor.ServiceInterface/JobExtensions.cs
@@ -7,11 +7,11 @@ namespace TalentBlazor.ServiceInterface;
 public static class JobExtensions
 {
     public static T WithAudit<T>(this T row, IRequest req, DateTime? date = null) where T : AuditBase =>
-        row.WithAudit(req, date);   
+        row.WithAudit(req.GetSession().UserAuthId, date);
 
     public static T WithAudit<T>(this T row, string by, DateTime? date = null) where T : AuditBase
     {
-        var useDate = date ?? DateTime.Now;
+        var useDate = date ?? DateTime.UtcNow;
         if (string.IsNullOrEmpty(row.CreatedBy))
         {
             row.CreatedBy = by;
