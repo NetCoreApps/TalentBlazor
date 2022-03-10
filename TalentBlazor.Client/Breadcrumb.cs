6f6c3b49ab4c55fd49aaee106ffba6612e3ec275
Darren Reid <darren@reidmail.org>
2022-03-11 10:55:13 +1100

    Breadcrumbs

---
+++ TalentBlazor.Client/Breadcrumb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TalentBlazor.Client
+{
+    public class Breadcrumb
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
