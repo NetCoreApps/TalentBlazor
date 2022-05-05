8be379d50bdce1678c17efe9d2b9ced2e1408ce1
Demis Bellot <demis.bellot@gmail.com>
2022-05-05 21:42:27 +0800

    Update FileUploadTests.cs

--- TalentBlazor.Tests/FileUploadTests.cs
+++ TalentBlazor.Tests/FileUploadTests.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 
 namespace TalentBlazor.Tests;
 
+[Category("Integration")]
 public class FileUploadTests
 {
     static string AppData = "../../../../TalentBlazor/App_Data";
