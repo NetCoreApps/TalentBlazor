426e848c3bee7af5933fc917e348de594e430154
Darren Reid <darren@reidmail.org>
2022-11-24 13:39:52 +1100

    Try fix CI build

--- TalentBlazor.Client/Auth/ServiceStackStateProvider.cs
+++ TalentBlazor.Client/Auth/ServiceStackStateProvider.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.Extensions.Logging;
 using ServiceStack;
 using ServiceStack.Blazor;
 
